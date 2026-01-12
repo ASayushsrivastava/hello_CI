7e1d4452283b07bb08bd390d0d7ff0a235b9e0b9
Ayush Srivastava <ayush.srivastava@admiralindia.com>
2026-01-13 05:09:08 +0530

    updated main method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, CI! from Ayush Srivastava");
+        System.out.println("Hello, CI!");
     }
 }
